596054ef2b22495ddd9ad6fdc0c36fe8b00d5bd6
jeel-patel-04 <161931618+jeel-patel-04@users.noreply.github.com>
2025-09-15 00:45:10 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+
 const Navbar = () => {
   return (
     <nav className='bg-purple-700 h-9 md:h-13 flex justify-between  px-2 md:px-15 items-center text-white'>
@@ -12,9 +14,9 @@ const Navbar = () => {
         <Link href="/generate"><li>Shortner</li></Link>
         <Link href="/contact"><li>Contact Us</li></Link> */}
         <div className="flex  justify-center gap-2">
-        <a href="https://github.com/jeel-patel-04" target="/"><img className="invert px-2  w-11 cursor-pointer" src="./github.png" alt="github" title="Github" /></a>
-        <a href="https://www.linkedin.com/in/jeel-patel-b0370b249/"target="/"><img className="invert px-2  w-11 cursor-pointer" src="./linkedin.png" alt="linkedin" title="Linkedin"/></a>
-        <a href="https://jeelspatel.netlify.app/" target="/"><img className="invert px-2  w-11 cursor-pointer" src="./aboutme.png" alt="developer" title="Developer"/></a>
+        <a href="https://github.com/jeel-patel-04" target="/"><Image className="invert px-2  w-11 cursor-pointer" src="./github.png" alt="github" title="Github" /></a>
+        <a href="https://www.linkedin.com/in/jeel-patel-b0370b249/"target="/"><Image className="invert px-2  w-11 cursor-pointer" src="./linkedin.png" alt="linkedin" title="Linkedin"/></a>
+        <a href="https://jeelspatel.netlify.app/" target="/"><Image className="invert px-2  w-11 cursor-pointer" src="./aboutme.png" alt="developer" title="Developer"/></a>
       </div>
       </ul>
     </nav>
@@ -22,3 +24,4 @@ const Navbar = () => {
 }
 
 export default Navbar
+
